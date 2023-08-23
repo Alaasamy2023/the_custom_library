e4ddf4cbed7997d5e2963b9ba59f10cf78edce6b
alaa samy <123264406+Alaasamy2023@users.noreply.github.com>
2023-08-24 00:23:39 +0300

    Add files via upload

---
+++ the_custom_library/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library",  # Module title
+    'sequence': -100,
+    'summary': "Manage books easily",  # Module subtitle phrase
+    'description': """
+Manage Library
+==============
+Description related to library.
+    """,  # Supports reStructuredText(RST) format
+    'author': "Parth Gajjar",
+    'website': "http://www.example.com",
+    'category': 'Tools',
+    'version': '14.0.1',
+    'depends': ['base'],
+    # This data files will be loaded at the installation (commented because file is not added in this example)
+    # 'data': [
+    #     'views.xml'
+    # ],
+    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml'
+    ],
+    'application': True,
+}
